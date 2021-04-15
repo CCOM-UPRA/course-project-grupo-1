00130120c9daf6d634ac98f55373f2381989dcdf
ElianAcevedo <elian.acevedo@upr.edu>
2021-04-14 21:37:42 -0400

    Sign_up

--- customerregister.php
+++ customerregister.php
@@ -16,7 +16,7 @@ if($password==$password2)
     $sql="INSERT INTO user(firstName, lastName,email,password, phonenumber) VALUES('$firstname','$lastname','$email',PASSWORD('$password'),'$phonenumber')";
     $connect->query($sql);
     
-    $sql2 ="INSERT INTO address(address_id, street1,street2,postal_code,city) VALUES('$address','$PO_box','$state','$country','$zip') FROM user WHERE usingID.address = usingID.user ";
+    $sql2 ="INSERT INTO address(address_id, street1,street2,postal_code,city) VALUES('$address','$PO_box','$state','$country','$zip')";
     $connect->query($sql2);
     echo "<script> alert('You are registered');
     window.location.href='login.php';
