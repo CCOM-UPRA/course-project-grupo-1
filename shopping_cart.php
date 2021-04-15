786aa5f9ad7bbdf7aef30f3c9c7f2ceff2ed1c0f
Ixan <ixan.melendez@upr.edu>
2021-04-15 14:40:10 -0400

    Update

--- shopping_cart.php
+++ shopping_cart.php
@@ -47,11 +47,9 @@ include("partials/header.php");
 include("partials/navbar.php");
 
 ?>
-   <!-- Navbar -->
-<?php
-include("functions/phpFunctions.php");
 
-?>
+
+
   <!-- Main Container -->
   <section class="main-container col1-layout" style="padding: 10px;">
     <div class="main container">
@@ -78,9 +76,10 @@ include("functions/phpFunctions.php");
                   </thead>
                   <tfoot>
                     <tr class="first last">
-                      <td class="a-right last" colspan="50"><button onclick="#" class="button btn-continue" title="Continue Shopping" type="button"><span>Continue Shopping</span></button>
-                        <button class="button btn-update" title="Update Cart" value="update_qty" name="update_cart_action" type="submit"><span>Update Cart</span></button>
-                        <button id="empty_cart_button" class="button" title="Clear Cart" value="empty_cart" name="update_cart_action" type="submit"><span>Clear Cart</span></button></td>
+                      <td class="a-right last" colspan="50"><button  class="button btn-continue" onclick = "index.php" title="Continue Shopping" type="submit"><span>Continue Shopping</span></button>
+
+                       <!-- <button class="button btn-update" title="Update Cart" value="update_qty" name="update_cart_action" type="submit"><span>Update Cart</span></button> -->
+                        <button id="empty_cart_button" class="button" title="Clear Cart" value="empty_cart" name="update_cart_action" onclick = "$('#shopping-cart-table tbody tr').remove()" type="submit"><span>Clear Cart</span></button></td>
                     </tr>
                   </tfoot>
 
@@ -96,7 +95,7 @@ include("functions/phpFunctions.php");
                       <td class="a-center hidden-table"><span class="cart-price"> <span class="price">$9.99</span> </span></td>
                       <td class="a-center movewishlist"><input maxlength="12" class="input-text qty" title="Qty" size="4" value="1" name=""></td>
                       <td class="a-center movewishlist"><span class="cart-price"> <span class="price">$9.99</span> </span></td>
-                      <td class="a-center last"><a class="button remove-item" title="Remove item" href="#"onClick="$(this).closest('tr').remove()"><span><span>Remove item</span></span></a></td>
+                      <td class="a-center last"><a class="button remove-item" title="Remove item" onClick="$(this).closest('tr').remove()"><span><span>Remove item</span></span></a></td>
                     </tr>
                     <tr class="last even">
                       <td class="image"><a class="product-image" title="" href="#"><img width="75" height="75" alt="Women's Crepe Printed Black" src="newImages/CartMovieImages/FarFromHomeMini.jpeg"></a></td>
@@ -277,223 +276,7 @@ include("partials/footer.php");
 ?>
     <!-- Footer  -->
 </div>
-<div id="mobile-menu">
-  <ul>
-    <li>
-      <div class="mm-search">
-        <form id="search_mob" name="search">
-          <div class="input-group">
-            <div class="input-group-btn">
-              <button class="btn btn-default" type="submit"><i class="fa fa-search"></i> </button>
-            </div>
-            <input type="text" class="form-control simple" placeholder="Search ..." name="srch-term" id="srch-term">
-          </div>
-        </form>
-      </div>
-    </li>
-    <li>
-      <div class="home"><a href="#"><i class="icon-home"></i>Home</a> </div>
-    </li>
-    <li><a href="#">Pages</a>
-      <ul>
-        <li><a href="grid.php">Grid</a> </li>
-        <li> <a href="list.php">List</a> </li>
-        <li> <a href="product_detail.php">Product Detail</a> </li>
-        <li> <a href="shopping_cart.php">Shopping Cart</a> </li>
-        <li><a href="checkout.php">Checkout</a>
-          <ul>
-            <li><a href="checkout_method.php">Checkout Method</a> </li>
-            <li><a href="checkout_billing_info.php">Checkout Billing Info</a> </li>
-          </ul>
-        </li>
-        <li> <a href="wishlist.php">Wishlist</a> </li>
-        <li> <a href="dashboard.php">Dashboard</a> </li>
-        <li> <a href="multiple_addresses.php">Multiple Addresses</a> </li>
-        <li> <a href="about_us.php">About us</a> </li>
-        <li><a href="blog.php">Blog</a>
-          <ul>
-            <li><a href="blog-detail.php">Blog Detail</a> </li>
-          </ul>
-        </li>
-        <li><a href="contact_us.php">Contact us</a> </li>
-        <li><a href="404error.php">404 Error Page</a> </li>
-      </ul>
-    </li>
-    <li><a href="#">Women</a>
-      <ul>
-        <li> <a href="#" class="">Stylish Bag</a>
-          <ul>
-            <li> <a href="#" class="">Clutch Handbags</a> </li>
-            <li> <a href="#l" class="">Diaper Bags</a> </li>
-            <li> <a href="#" class="">Bags</a> </li>
-            <li> <a href="#" class="">Hobo handbags</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Material Bag</a>
-          <ul>
-            <li> <a href="#">Beaded Handbags</a> </li>
-            <li> <a href="#">Fabric Handbags</a> </li>
-            <li> <a href="#">Handbags</a> </li>
-            <li> <a href="#">Leather Handbags</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Shoes</a>
-          <ul>
-            <li> <a href="#">Flat Shoes</a> </li>
-            <li> <a href="#">Flat Sandals</a> </li>
-            <li> <a href="#">Boots</a> </li>
-            <li> <a href="#">Heels</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Jwellery</a>
-          <ul>
-            <li> <a href="#">Bracelets</a> </li>
-            <li> <a href="#">Necklaces &amp; Pendent</a> </li>
-            <li> <a href="#l">Pendants</a> </li>
-            <li> <a href="#">Pins &amp; Brooches</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Dresses</a>
-          <ul>
-            <li> <a href="#">Casual Dresses</a> </li>
-            <li> <a href="#">Evening</a> </li>
-            <li> <a href="#">Designer</a> </li>
-            <li> <a href="#">Party</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Swimwear</a>
-          <ul>
-            <li> <a href="#">Swimsuits</a> </li>
-            <li> <a href="#">Beach Clothing</a> </li>
-            <li> <a href="#">Clothing</a> </li>
-            <li> <a href="#">Bikinis</a> </li>
-          </ul>
-        </li>
-      </ul>
-    </li>
-    <li><a href="#">Men</a>
-      <ul>
-        <li> <a href="#" class="">Shoes</a>
-          <ul class="level1">
-            <li class="level2 nav-6-1-1"><a href="#">Sport Shoes</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Casual Shoes</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Leather Shoes</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">canvas shoes</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Dresses</a>
-          <ul class="level1">
-            <li class="level2 nav-6-1-1"><a href="#">Casual Dresses</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Evening</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Designer</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Party</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Jackets</a>
-          <ul class="level1">
-            <li class="level2 nav-6-1-1"><a href="#">Coats</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Formal Jackets</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Leather Jackets</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Blazers</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Watches</a>
-          <ul class="level1">
-            <li class="level2 nav-6-1-1"><a href="#">Fasttrack</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Casio</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Titan</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Tommy-Hilfiger</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Sunglasses</a>
-          <ul class="level1">
-            <li class="level2 nav-6-1-1"><a href="#">Ray Ban</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Fasttrack</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Police</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Oakley</a> </li>
-          </ul>
-        </li>
-        <li> <a href="#">Accesories</a>
-          <ul class="level1">
-            <li class="level2 nav-6-1-1"><a href="#">Backpacks</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Wallets</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Laptops Bags</a> </li>
-            <li class="level2 nav-6-1-1"><a href="#">Belts</a> </li>
-          </ul>
-        </li>
-      </ul>
-    </li>
-    <li><a href="#">Electronics</a>
-      <ul>
-        <li> <a href="#"><span>Mobiles</span></a>
-          <ul>
-            <li> <a href="#"><span>Samsung</span></a> </li>
-            <li> <a href="#"><span>Nokia</span></a> </li>
-            <li> <a href="#"><span>IPhone</span></a> </li>
-            <li> <a href="#"><span>Sony</span></a> </li>
-          </ul>
-        </li>
-        <li> <a href="#" class=""><span>Accesories</span></a>
-          <ul>
-            <li> <a href="#"><span>Mobile Memory Cards</span></a> </li>
-            <li> <a href="#"><span>Cases &amp; Covers</span></a> </li>
-            <li> <a href="#"><span>Mobile Headphones</span></a> </li>
-            <li> <a href="#"><span>Bluetooth Headsets</span></a> </li>
-          </ul>
-        </li>
-        <li> <a href="#"><span>Cameras</span></a>
-          <ul>
-            <li> <a href="#"><span>Camcorders</span></a> </li>
-            <li> <a href="#"><span>Point &amp; Shoot</span></a> </li>
-            <li> <a href="#"><span>Digital SLR</span></a> </li>
-            <li> <a href="#"><span>Camera Accesories</span></a> </li>
-          </ul>
-        </li>
-        <li> <a href="#"><span>Audio &amp; Video</span></a>
-          <ul>
-            <li> <a href="#"><span>MP3 Players</span></a> </li>
-            <li> <a href="#"><span>IPods</span></a> </li>
-            <li> <a href="#"><span>Speakers</span></a> </li>
-            <li> <a href="#"><span>Video Players</span></a> </li>
-          </ul>
-        </li>
-        <li> <a href="#"><span>Computer</span></a>
-          <ul>
-            <li> <a href="#"><span>External Hard Disk</span></a> </li>
-            <li> <a href="#"><span>Pendrives</span></a> </li>
-            <li> <a href="#"><span>Headphones</span></a> </li>
-            <li> <a href="#"><span>PC Components</span></a> </li>
-          </ul>
-        </li>
-        <li> <a href="#"><span>Appliances</span></a>
-          <ul>
-            <li> <a href="#"><span>Vaccum Cleaners</span></a> </li>
-            <li> <a href="#"><span>Indoor Lighting</span></a> </li>
-            <li> <a href="#"><span>Kitchen Tools</span></a> </li>
-            <li> <a href="#"><span>Water Purifier</span></a> </li>
-          </ul>
-        </li>
-      </ul>
-    </li>
-    <li><a href="#">Furniture</a> </li>
-    <li><a href="#">Kids</a> </li>
-    <li><a href="contact-us.php">Contact Us</a> </li>
-  </ul>
-  <div class="dropdown block-language-wrapper">
-                <ul>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><img src="images/english.png" alt="language"> English </a></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><img src="images/francais.png" alt="language"> French </a></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><img src="images/german.png" alt="language"> German </a></li>
-                </ul>
-              </div>
-              <div class="dropdown block-currency-wrapper">
-                <ul>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"> $ - Dollar </a></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"> £ - Pound </a></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"> € - Euro </a></li>
-                </ul>
-              </div>
-</div>
+
 <!-- JavaScript -->
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
