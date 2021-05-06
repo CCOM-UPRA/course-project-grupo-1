d8d57060a269a556d48a47bd43f54ff726d74b32
Cristopher Rosado <cristopher.rosado@upr.edu>
2021-05-05 19:18:48 -0700

    Cart files

---
+++ includes/classes/Cart.php
@@ -0,0 +1,17 @@
+<?php
+	class Account {
+        
+        private $con;
+		private $errorArray;
+
+		public function __construct($con) {
+			$this->con = $con;
+			$this->errorArray = array();
+		}
+    
+    
+    
+    
+    
+    
+    }
